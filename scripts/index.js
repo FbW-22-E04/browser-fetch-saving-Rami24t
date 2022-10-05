6eefd09c8a94f6dc6e3196d7557b32babe1e7381
RamS24 <AlSaadi.Rami24@gmail.com>
2022-10-05 16:23:17 +0200

    solution part 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,35 @@
+'use strict';
 import View from './View.js';
 import Client from './Client.js';
 const view = new View();
 const client = new Client();
 const list = [];
 if (localStorage.getItem('list'))
-    list.push(...localStorage.getItem('list'));
-for (movie of list) {
+{
+    const savedList = JSON.parse(localStorage.getItem('list'));
+    list.push(...savedList);
+}
+for (const movie of list) {
     client.getMovieData(movie).then(data =>
         view.displayMovieOnPage(data))
 }
 document.querySelector('.buttons').addEventListener('click', e => {
     if (e.target.classList.contains('btn-save')) {
-        localStorage.setItem('list', list);
+        localStorage.setItem('list', JSON.stringify(list));
     }
     else if (e.target.classList.contains('btn-reset')) {
         list.length = 0;
         localStorage.removeItem('list');
     }
 })
-input = document.querySelector('#input');
-document.querySelector('#input').addEventListener('keypress', e => {
+const input = document.querySelector('#input');
+input.addEventListener('keypress', e => {
     if (e.key == 'Enter') {
         let movie = input.value;
         if (movie) {
             client.getMovieData(movie)
                 .then(data => {
-                    list.push(data);
+                    list.push(movie);
                     view.displayMovieOnPage(data);
                 });
         }
